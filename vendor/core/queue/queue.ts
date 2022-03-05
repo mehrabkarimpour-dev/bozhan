a1e5414961cd46abda18d7d7f79b3f3c1d656de3
Mehrab Karimpour <mehrab.karimpour@elanza.com>
2022-03-05 17:40:34 +0330

    feat (converting handle method in queues to the abstract and taking it to queue.ts)

--- vendor/core/queue/queue.ts
+++ vendor/core/queue/queue.ts
@@ -1,7 +1,9 @@
-class Queue{
+abstract class Queue {
     public delay: number = 0 // one second
     public removeOnComplete: boolean = true
     public attempts: number = 2
+
+    abstract handle(args: any): any;
 }
 
 export default Queue
