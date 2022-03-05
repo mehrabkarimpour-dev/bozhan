a55f568614120aa8878bd9f643b1c14c3d967645
karen <mehrab0karimpour@gmail.com>
2022-03-06 00:49:54 +0330

    feat (adding cli maker , first event maker)

--- app/console/command/command.ts
+++ app/console/command/command.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const {exec} = require('child_process')
-
+let fs = require('fs');
 let program = require('commander');
 const chalk = require('chalk');
 const figlet = require('figlet');
@@ -18,9 +18,18 @@ program
     .action(function () {
         console.log('run')
     })
-program.command('make')
-    .action(function () {
-        console.log('make')
+
+
+program.command('make <type>')
+    .action(async function (args) {
+        let argsArray = args.split(':')
+        exec('touch app/events/published/' + argsArray[1] + '.ts')
+        let event = "import {Dispatchable} from '../../../vendor/core/event/Dispatchable';\n\nexport class "+argsArray[1]+" extends Dispatchable {\n\n    public name: string = '"+argsArray[1]+"'\n\n\n    public registerListeners()\n{\n        return [\n\n        ]\n\n    }\n\n    public run(...parameters: any) {\n\n    }\n\n}"
+        fs.writeFile('app/events/published/' + argsArray[1] + '.ts', event, function (err) {
+            if (err)
+                return console.log(err);
+            console.log('Wrote Hello World in file helloworld.txt, just check it');
+        })
     })
 
 program.parse()
