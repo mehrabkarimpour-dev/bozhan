48cc12b5c2f4d45fa0b7058c637f5f1ea1ed737f
mehrabkarimpour-dev <mehrab0karimpour@gmail.com>
2022-04-03 11:52:20 +0430

    fix (remove sql log in sequelize orm )

--- config/config/config.js
+++ config/config/config.js
@@ -6,20 +6,23 @@ module.exports = {
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_DATABASE,
         "host":process.env.DB_HOST ||  "127.0.0.1",
-        "dialect": process.env.DB_DIALECT ||  "mysql"
+        "dialect": process.env.DB_DIALECT ||  "mysql",
+        logging: false
     },
     "test": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_DATABASE,
         "host": process.env.DB_HOST ||  "127.0.0.1",
-        "dialect": process.env.DB_DIALECT ||  "mysql"
+        "dialect": process.env.DB_DIALECT ||  "mysql",
+        logging: false
     },
     "production": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_DATABASE,
         "host": process.env.DB_HOST || "127.0.0.1",
-        "dialect":process.env.DB_DIALECT || "mysql"
+        "dialect":process.env.DB_DIALECT || "mysql",
+        logging: false
     }
 }
